a3eccb35daeb721f6053873b9cc04129dbc43663
Mircho Mirev <m.mirev@allterco.com>
2023-09-17 01:10:44 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Purpose
+
+C abstraction for sensor data filtering.
+
+## References
+
+- [Least squares article](https://www.technologynetworks.com/informatics/articles/calculating-a-least-squares-regression-line-equation-example-explanation-310265)
---
+++ main.c
@@ -0,0 +1,156 @@
+#include "stdio.h"
+#include "sensor.h"
+
+void vo_1(observable_value_t *this) 
+{
+  printf("Value observer (1) %.3f\n", this->value.value);
+}
+
+void vo_2(observable_value_t *this)
+{
+  printf("Value observer (2) %f\n", this->value.value);
+}
+
+void test_generic(void) {
+  OBSERVABLE_VALUE(pressure, 0);
+
+  ADD_OBSERVER(pressure, vo_1);
+  ADD_OBSERVER(pressure, vo_2);
+
+  NOTIFY_OBSERVERS(pressure);
+  REMOVE_OBSERVER(pressure, vo_1);
+  NOTIFY_OBSERVERS(pressure);
+  // REMOVE_OBSERVER(pressure, vo_2);
+  // NOTIFY_OBSERVERS(pressure);
+
+  FILTER_CLAMP(clamp_pressure_adc, 500, 700);
+  FILTER_OFFSET(offset_pressure, -30);
+  FILTER_SKIP(skip_values, 3);
+
+  ADD_FILTER(pressure, skip_values);
+  ADD_FILTER(pressure, clamp_pressure_adc);
+  ADD_FILTER(pressure, offset_pressure);
+
+  OBSERVABLE_NUMBER(my_var, 0);
+
+  SET_OBSERVABLE_VAR(my_var, 200);
+  PROCESS_NEW_VALUE(pressure, my_var);
+
+  SET_OBSERVABLE_VAR(my_var, 400);
+  PROCESS_NEW_VALUE(pressure, my_var);
+
+  SET_OBSERVABLE_VAR(my_var, 600);
+  PROCESS_NEW_VALUE(pressure, my_var);
+
+  SET_OBSERVABLE_VAR(my_var, 590);
+  PROCESS_NEW_VALUE(pressure, my_var);
+
+  SET_OBSERVABLE_VAR(my_var, 720);
+  PROCESS_NEW_VALUE(pressure, my_var);
+
+  SET_OBSERVABLE_VAR(my_var, 620);
+  PROCESS_NEW_VALUE(pressure, my_var);
+
+  CLEANUP_OBSERVERS(pressure);
+}
+
+void test_exp_m_avg(void) {
+  double test_values[] = {
+    500,
+    510,
+    520,
+    530,
+    540,
+    550,
+    560,
+    570,
+    580,
+  };
+
+  OBSERVABLE_VALUE(pressure_ema, 0);
+  ADD_OBSERVER(pressure_ema, vo_1);
+
+  FILTER_EXP_MOVING_AVERAGE(ema_pressure_filter, 0.8, true);
+  ADD_FILTER(pressure_ema, ema_pressure_filter);
+
+  OBSERVABLE_NUMBER(ea_var, 0);
+
+  for(size_t cnt = 0; cnt < (sizeof(test_values)/sizeof(test_values[0])); cnt++) {
+    SET_OBSERVABLE_VAR(ea_var, test_values[cnt]);
+    PROCESS_NEW_VALUE(pressure_ema, ea_var);
+  }
+
+  printf("Result for EMA: %.4f\n", pressure_ema.value.value);
+
+  CLEANUP_OBSERVERS(pressure_ema);
+}
+
+void test_avg(void) {
+  double test_values[] = {
+    1,
+    2,
+    3,
+    10,
+    20,
+    30,
+    100,
+    200,
+    300,
+    1000,
+    2000,
+    3000,
+  };
+
+  OBSERVABLE_VALUE(pressure_a, 0);
+  ADD_OBSERVER(pressure_a, vo_1);
+
+  FILTER_AVERAGE(a_pressure_filter, 3, false);
+  ADD_FILTER(pressure_a, a_pressure_filter);
+
+  OBSERVABLE_NUMBER(a_var, 0);
+
+  for(size_t cnt = 0; cnt < (sizeof(test_values)/sizeof(test_values[0])); cnt++) {
+    SET_OBSERVABLE_VAR(a_var, test_values[cnt]);
+    PROCESS_NEW_VALUE(pressure_a, a_var);
+  }
+
+  printf("Result for A: %.4f\n", pressure_a.value.value);
+
+  CLEANUP_OBSERVERS(pressure_a);
+}
+
+void test_linear_fit(void) {
+
+  OBSERVABLE_VALUE(pressure_lf, 0);
+  ADD_OBSERVER(pressure_lf, vo_1);
+
+//  FILTER_LINEAR_FIT(a_pressure_filter, 1,1,10,50);
+  FILTER_LINEAR_FIT(a_pressure_filter, 500,0,685,100);
+  printf("Slope: %f, Intercept: %f\n", a_pressure_filter.slope_, a_pressure_filter.intercept_);
+  ADD_FILTER(pressure_lf, a_pressure_filter);
+
+  FILTER_CLAMP(a_pressure_filter_c, 0, 100);
+  ADD_FILTER(pressure_lf, a_pressure_filter_c);
+
+
+  OBSERVABLE_NUMBER(a_var, 550);
+  PROCESS_NEW_VALUE(pressure_lf, a_var);
+
+  printf("Result for A: %.4f\n", pressure_lf.value.value);
+
+  // expected 27.778
+  CLEANUP_OBSERVERS(pressure_lf);
+}
+
+int main(void)
+{
+  test_generic();
+
+  test_exp_m_avg();
+
+  test_avg();
+
+  test_linear_fit();
+
+  return 0;
+}
